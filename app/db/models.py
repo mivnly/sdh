112a55c9736f1351409fa5efb5b17c8233f8b86e
cgntvld <ivanmorozovpage@yandex.ru>
2026-02-25 20:10:10 +0300

    Update users model fields: name, surname, username, role

--- app/db/models.py
+++ app/db/models.py
@@ -11,7 +11,14 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(BigInteger, Identity(), primary_key=True)
-    name: Mapped[str] = mapped_column(String(80), nullable=False)
-    surname: Mapped[str] = mapped_column(String(80), nullable=False)
-    role: Mapped[str] = mapped_column(String(20), nullable=False)
+    name: Mapped[str] = mapped_column(String(80), nullable=True)
+    surname: Mapped[str] = mapped_column(String(80), nullable=True)
+    username: Mapped[str] = mapped_column(String(80), nullable=False, unique=True)
+    role: Mapped[str] = mapped_column(String(20), nullable=False, default='user', server_default='user')
     comment: Mapped[str] = mapped_column(Text, nullable=True)
+
+    def __repr__(self):
+        return (
+            f"{self.id} | {self.name} | {self.surname} | {self.username} | "
+            f"{self.role} | {self.comment}"
+        )
