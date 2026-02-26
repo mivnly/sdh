32d38a87d7f5c65ebf81f47ca19936d785c02389
cgntvld <ivanmorozovpage@yandex.ru>
2026-02-26 20:07:47 +0300

    Add get_session() function for Depends() of FastAPI

--- app/db/config.py
+++ app/db/config.py
@@ -1,15 +1,22 @@
+from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.config import settings
 
-engine = create_async_engine(
+
+_engine = create_async_engine(
     settings.conn_url.encoded_string(),
     echo=False,
     pool_pre_ping=True
 )
 
 asession_maker = async_sessionmaker(
-    bind=engine,
+    bind=_engine,
     class_=AsyncSession,
-    expire_on_commit=False
+    expire_on_commit=False,
+    autoflush=False
 )
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with asession_maker() as asession:
+        yield asession
