d8dc49161bbfb3e6e5795e05d79bf3e5cffcefaa
cgntvld <ivanmorozovpage@yandex.ru>
2026-02-27 18:34:17 +0300

    Minor fixes

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -3,15 +3,18 @@ from pydantic import BaseModel, ConfigDict
 class UserBase(BaseModel):
     name: str | None = None
     surname: str | None = None
-    username: str
     comment: str | None = None
 
 class UserCreate(UserBase):
+    username: str
     role: str = "user"
     
 class UserRead(UserBase):
+    id: int | None = None
+    username: str
     role: str = "user"
     model_config = ConfigDict(from_attributes=True)
 
 class UserUpdate(UserBase):
+    username: str | None = None
     role: str | None = None
