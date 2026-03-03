7b30bcf67502c82f18569bc2ffe527e11ab385c5
cgntvld <ivanmorozovpage@yandex.ru>
2026-03-03 14:22:05 +0300

    Refactor log initialization. Add class with urls used in app

--- app/config.py
+++ app/config.py
@@ -1,22 +1,16 @@
-import logging
-
-from pydantic import PostgresDsn, SecretStr, computed_field
+from pydantic import BaseModel, ConfigDict, PostgresDsn, SecretStr, computed_field
+from pydantic.networks import HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from common.utils import setup_logger
+
 # === Logger ===
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(filename)s::%(funcName)s | %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
-)
-
-log = logging.getLogger("app")
+log = setup_logger("app", "app.log")
 
 
 # === DB ===
-
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="forbid", case_sensitive=True)
 
     DB_HOST: str
     DB_NAME: str
@@ -37,11 +31,21 @@ class Settings(BaseSettings):
         )
         return obj
 
-    model_config = SettingsConfigDict(
-        env_file='.env',
-        env_file_encoding='utf-8',
-        extra="forbid",
-        case_sensitive=True
-    )
 
-settings = Settings() #type: ignore
+settings = Settings()  # type: ignore
+
+
+# === URLS ===
+class AppUrls(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    base_url: HttpUrl
+    api_path: str
+
+    @computed_field
+    @property
+    def api_url(self) -> HttpUrl:
+        return HttpUrl(self.base_url.encoded_string() + self.api_path)
+
+
+app_urls = AppUrls(base_url=HttpUrl("http://127.0.0.1:8000/"), api_path="api/v1")
