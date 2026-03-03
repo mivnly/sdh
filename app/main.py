8810b5c8b637be3fc21f1b1769bde2aea78ffc24
cgntvld <ivanmorozovpage@yandex.ru>
2026-03-03 14:23:58 +0300

    Add '/api/v1' prefix to routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
-from app.routers.users import router as users_router
+from fastapi import APIRouter, FastAPI
+
+from app.routers.common import common_router
+from app.routers.users import users_router
+
+api_router = APIRouter(prefix="/api/v1")
+api_router.include_router(common_router)
+api_router.include_router(users_router)
 
 app = FastAPI()
-
-app.include_router(users_router)
+app.include_router(api_router)
